107275faae42666cbcd9f0e18789e545e10e64bf
Semmieeeh <semleungen05@gmail.com>
2023-02-17 13:20:06 +0100

    Refill mechanic

--- Periode 3/Assets/MachineScript.cs
+++ Periode 3/Assets/MachineScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MachineScript : MonoBehaviour
 {
     public GameObject objectToPaint;
+    public int refills;
+    bool refilled;
     
     void Start()
     {
@@ -17,24 +19,65 @@ public class MachineScript : MonoBehaviour
         
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Refill")
+        {
+            Refill();
+        }
+    }
+    public void Refill()
+    {
+        if(refilled == false)
+        {
+            refills = 10;
+            refilled = true;
+        }
+    }
     public void ColorGreen()
     {
-        Renderer rend = objectToPaint.GetComponent<Renderer>();
-        Color color = Color.green;
-        rend.material.color = color;
+        if (refills > 0)
+        {
+            Renderer rend = objectToPaint.GetComponent<Renderer>();
+            Color color = Color.green;
+            rend.material.color = color;
+            refills -= 1;
+        }
     }
 
     public void ColorRed()
     {
-        Renderer rend = objectToPaint.GetComponent<Renderer>();
-        Color color = Color.red;
-        rend.material.color = color;
+        if(refills > 0)
+        {
+            Renderer rend = objectToPaint.GetComponent<Renderer>();
+            Color color = Color.red;
+            rend.material.color = color;
+            refills -= 1;
+        }
     }
 
     public void ColorBlue()
     {
-        Renderer rend = objectToPaint.GetComponent<Renderer>();
-        Color color = Color.blue;
-        rend.material.color = color;
+        if(refills > 0)
+        {
+            Renderer rend = objectToPaint.GetComponent<Renderer>();
+            Color color = Color.blue;
+            rend.material.color = color;
+            refills -= 1;
+        }
     }
+
+    public void ColorMagenta()
+    {
+        if (refills > 0)
+        {
+            Renderer rend = objectToPaint.GetComponent<Renderer>();
+            Color color = Color.magenta;
+            rend.material.color = color;
+            refills -= 1;
+        }
+    }
+
+
+
 }
